d1467e0ecfcd1977fbae70122b6e978cd426b34f
RosalindFok <huoyufu19@mails.ucas.ac.cn>
2024-02-22 14:06:28 +0800

    path in blip.py

--- models/blip_override/blip.py
+++ models/blip_override/blip.py
@@ -182,7 +182,7 @@ def blip_feature_extractor(pretrained='', **kwargs):
 
 
 def init_tokenizer():
-    tokenizer = BertTokenizer.from_pretrained(os.path.join('..','..','..','bert-base-uncased'))
+    tokenizer = BertTokenizer.from_pretrained('/lustre/S/yuxiaoyi/bert-base-uncased')
     tokenizer.add_special_tokens({'bos_token': '[DEC]'})
     tokenizer.add_special_tokens({'additional_special_tokens': ['[ENC]']})
     tokenizer.enc_token_id = tokenizer.additional_special_tokens_ids[0]
