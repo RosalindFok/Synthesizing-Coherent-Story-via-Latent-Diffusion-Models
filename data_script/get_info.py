6cfc529e3191b68f539fd7eb8e679f70597cf8d7
RosalindFok <huoyufu19@mails.ucas.ac.cn>
2024-03-09 07:59:18 +0800

    only one item in test

--- data_script/get_info.py
+++ data_script/get_info.py
@@ -1,5 +1,6 @@
 import os
 import json
+import h5py
 
 split_result_path = os.path.join('..', '..', 'dataset', 'split')
 dirs_path_list = [os.path.join(split_result_path, x) for x in os.listdir(split_result_path) if os.path.isdir(os.path.join(split_result_path, x))]
@@ -9,3 +10,10 @@ for dir in dirs_path_list:
     assert len(files) == 1
     with open(files[0]) as f:
         data = json.load(f)
+
+hdf5_dir = os.path.join('..', '..', 'oxford_data')
+hdf5_path = os.path.join(hdf5_dir, 'oxford.hdf5')
+
+with h5py.File(hdf5_path, 'r') as f:
+    for group in f['test']:
+        print(f'group = {group}')
