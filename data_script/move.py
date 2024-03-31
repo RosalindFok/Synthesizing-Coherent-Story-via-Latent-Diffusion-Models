eb72817d9b0b7991bc89604f56ebaac65f454a01
RosalindFok <huoyufu19@mails.ucas.ac.cn>
2024-03-31 22:44:25 +0800

    bug_fixed

--- data_script/move.py
+++ data_script/move.py
@@ -14,7 +14,7 @@ with open('caption.json', 'r') as f:
 newcaps_path = os.path.join('..', '..', 'dataset', 'newcaps')
 create_dir(newcaps_path)
 for dir in tqdm(dirs_path_list, desc='Moving', leave=True):
-    image_path = [os.path.join(dir, x) for x in os.listdir(dir) if not '.json' in x]
+    image_path = [os.path.join(dir, x) for x in os.listdir(dir) if not '.json' in x] if not 'test' in dir else [os.path.join(dir, x) for x in os.listdir(dir)] 
     dir = os.path.join(newcaps_path, dir.split(os.sep)[-1])
     create_dir(dir)
     caption = {}
@@ -22,9 +22,8 @@ for dir in tqdm(dirs_path_list, desc='Moving', leave=True):
         image_name = image.split(os.sep)[-1]
         shutil.copy(src=image, dst=os.path.join(dir, image_name))
         # Do not change caption in test
-        if 'test' in dir:
-            if image_name in caption:
-                caption[image_name] = caption[image_name]
+        if '.json' in image:
+            continue
         else:
             caption[image_name] = data[image_name]
     
